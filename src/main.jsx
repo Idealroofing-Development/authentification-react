1421bdb884100c6c326a73496956ceb50fedbdea
HichemNgz <neggazhichem@gmail.com>
2024-04-30 15:07:43 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { AppProviders } from "./context";
+import { AppProviders } from "./context/index.jsx";
 import { Profiler } from "./utils/Profiler.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
